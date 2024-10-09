1d864b97d789208fb2f71940f81811999a3afb02
pestak <you@example.com>
2024-10-09 17:12:32 +0200

    debut de l'exercice2-variables

---
+++ TD1/exo1-oujs/index.js
@@ -0,0 +1,7 @@
+/*
+Un commentaire sur
+plusieurs lignes
+
+*/
+
+const pElt = document.getElementById('paragraphe'); //commentaire
---
+++ TD1/exo2-variables/index.js
@@ -0,0 +1,26 @@
+var prenom = "georges" //on ne l'utilise plus
+const prenom2 = 'georgette' //constante
+let prenom3 = 'jean' //variable
+
+age = 21
+
+
+console.log(typeof(prenom))
+console.log(typeof(age))
+console.log(typeof(2 == 2))
+console.log(2 == '2') //vrai
+console.log(2 === '2') //faux car pas le même type
+
+console.log(2*3) //6
+console.log(13%2) //1 pour connaitre si un nombre est divisible par autre
+
+let a = 2;
+a++;
+console.log(a);
+a /=  3
+console.log(a)
+
+
+console.log('Bonjour ' + prenom);
+
+console.log('Bonjour  ${prenom} . Comment ça va ?')
