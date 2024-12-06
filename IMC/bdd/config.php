07f3c1af3f89fa7a17b9dac07d0c4bc9d9205dd1
David LEGRAND <wawawaformation@gmail.com>
2024-12-06 12:44:03 +0100

    activation menu burger

---
+++ IMC/bdd/config.php
@@ -0,0 +1,4 @@
+<?php
+/**
+ * infos confidentiel
+ */
---
+++ IMC/public/js/burger.js
@@ -0,0 +1,3 @@
+document.getElementById('burger').addEventListener('click',function(){
+    document.querySelector('.menu-principal').classList.toggle('showme')
+})
